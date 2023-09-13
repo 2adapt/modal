6d8a264b9ad43903013f84caff040ba761b5dc2f
ruipns <ruipns@live.com.pt>
2023-09-13 15:41:04 +0100

    Fix2

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,4 +1,7 @@
 // Reexport your entry components here
 import Modal from './Modal.svelte';
+import { showModal2 } from './Modal.svelte';
+import { closeModal2 } from './Modal.svelte';
 
-export { Modal };
+
+export { Modal, showModal2, closeModal2 };
